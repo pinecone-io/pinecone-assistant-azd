a8ae139df0d71ce87230ab35020d395559b7b9df
Cory Waddingham <cory@pinecone.io>
2024-08-22 09:38:41 -0700

    Added logic to ensure AZURE_ENV_NAME is set, addresses https://github.com/pinecone-io/pinecone-assistant-azd/issues/6

--- .azd/hooks/postprovision.sh
+++ .azd/hooks/postprovision.sh
@@ -59,6 +59,18 @@ merge_env_files() {
 script_dir="$(dirname "$(readlink -f "$0")")"
 
 env_local="$script_dir/../../.env.local"
+if [ -z $AZURE_ENV_NAME ]; then
+    # The Azure environment hasn't been set yet. Check if there's a directory under .azure and use that. Otherwise, prompt for one.
+    num_envs=$(ls .azure/ | grep -v -e .gitignore -e config.json | wc -l)
+    if [ $num_envs = 1 ];then
+        export AZURE_ENV_NAME=`ls .azure/ | grep -v -e .gitignore -e config.json | sed -e 's/\///'`
+    else
+        echo -n "Unable to determine AZURE_ENV_NAME, please provide it: "
+        read value
+        export AZURE_ENV_NAME=$value
+    fi
+fi
+
 env_azd="$script_dir/../../.azure/${AZURE_ENV_NAME}/.env"
 
 env_azure="$script_dir/../../.env.azure"
