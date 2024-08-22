45e1a29114c116fdba4e3e9f4eba76af71eee4c6
Cory Waddingham <cory@pinecone.io>
2024-08-22 08:47:08 -0700

    Revised Pyton scripts to have local path in PYTHONPATH to address https://github.com/pinecone-io/pinecone-assistant-azd/issues/7

--- src/file_manager/initialize_assistant.py
+++ src/file_manager/initialize_assistant.py
@@ -1,3 +1,11 @@
+import os
+import sys
+
+# Add the project root to PYTHONPATH
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
+
 from file_manager import *
 import os, time
 import logging
